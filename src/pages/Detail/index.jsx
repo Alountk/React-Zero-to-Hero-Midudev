36790a6845119910a643b55fa92f3829ff4135b6
Raul <alountk@gmail.com>
2020-09-11 18:08:46 +0200

    create new hook useGlobalGif

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,14 +1,13 @@
-import React, {useContext} from 'react'
-import GifsContext from '../../context/GifsContext'
+import React from 'react'
+import useGlobalGifs from '../../hooks/useGlobalGifs'
 import Gif from '../../components/Gif/Gif';
 
 export default function Detail({params:{id}}) {
-    const {gifs} = useContext(GifsContext);
+    const gifs = useGlobalGifs();
     const gif = gifs.find(singleGif => singleGif.id === id);
-    const {title, url} = gif;
     return (
         <div>
-           <Gif id={id} title={title} url={url}/> 
+           <Gif {...gif}/> 
         </div>
     )
 }
